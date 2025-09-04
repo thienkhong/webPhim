40ab187a80bb7a6907e0972142fe042619f7dfff
Kayty <khongthien2805@gmail.com>
2025-09-05 01:08:31 +0700

    Revert "update"
    This reverts commit c7e11525c18de9b835bfe569e92bcfc398b82343.

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,26 +2,26 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return(
         <header className="header">
-            <div className="header-nav max-width-1500">
-                <Link to="/" className="logo">PhimMoi</Link>
-                <nav className="main-nav">
-                    <ul>
-                        <li><Link to="/">PHIMMOI</Link></li>
-                        <li><Link to="/phimle">PHIM LẺ</Link></li>
-                        <li><Link to="/phimbo">PHIM BỘ</Link></li>
-                        {/* <li><Link to="/theloai">THỂ LOẠI</Link></li>
-                        <li><Link to="/quocgia">QUỐC GIA</Link></li>
-                        <li><Link to="/namphathanh">NĂM PHÁT HÀNH</Link></li> */}
-                        <li><Link to="/phimchieurap">PHIM CHIẾU RẠP</Link></li>
-                        {/* <li><Link to="/trailer">TRAILER</Link></li> */}
-                        <li><Link to="/topphim">TOP PHIM</Link></li>
-                    </ul>
-                </nav>
-                <div className="search-box">
-                    <input type="text" placeholder="Tìm tên phim, diễn viên..."/>
-                    <button><i className="fas fa-search"></i></button>
-                </div>
+        <div className="header-nav max-width-1500">
+            <Link to="/" className="logo">PhimMoi</Link>
+            <nav className="main-nav">
+                <ul>
+                    <li><Link to="/">PHIMMOI</Link></li>
+                    <li><Link to="/phimle">PHIM LẺ</Link></li>
+                    <li><Link to="/phimbo">PHIM BỘ</Link></li>
+                    {/* <li><Link to="/theloai">THỂ LOẠI</Link></li>
+                    <li><Link to="/quocgia">QUỐC GIA</Link></li>
+                    <li><Link to="/namphathanh">NĂM PHÁT HÀNH</Link></li> */}
+                    <li><Link to="/phimchieurap">PHIM CHIẾU RẠP</Link></li>
+                    {/* <li><Link to="/trailer">TRAILER</Link></li> */}
+                    <li><Link to="/topphim">TOP PHIM</Link></li>
+                </ul>
+            </nav>
+            <div className="search-box">
+                <input type="text" placeholder="Tìm tên phim, diễn viên..."/>
+                <button><i className="fas fa-search"></i></button>
             </div>
-        </header>
+        </div>
+    </header>
     )
 }
